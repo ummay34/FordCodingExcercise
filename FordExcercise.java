d80fdb86d78b5ee7ef735cbb0ba2119fdfc645c6
Ummayair Ahmad <45613172+ummay34@users.noreply.github.com>
2021-03-12 21:57:09 -0500

    slight changes

--- FordExcercise.java
+++ FordExcercise.java
@@ -18,19 +18,19 @@ public class FordExcercise {
 
                 //if the number is divisible by both 3 and 5 print MustangBronco
                 if (inputNum % 3 == 0 && inputNum % 5 == 0) {
-                    System.out.println("MustangBronco");
+                    System.out.println("MustangBronco\n");
 
                     //If the number is divisible by 3 print Mustang
                 } else if (inputNum % 3 == 0) {
-                    System.out.println("Mustang");
+                    System.out.println("Mustang\n");
 
                     //If the number is divisible by 5 print bronco
                 } else if (inputNum % 5 == 0) {
-                    System.out.println("Bronco");
+                    System.out.println("Bronco\n");
 
                     //For every other number print the number itself
                 } else {
-                    System.out.println(inputNum);
+                    System.out.println(inputNum + "\n");
                 }
             }
             //If user enters q, quit the program
